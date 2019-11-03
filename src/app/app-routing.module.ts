33b64049c84590ecaf0e82cdb49bc886f6ada4f2
Leo Sormunen <Leo.Sormunen@student.laurea.fi>
2019-11-04 00:40:37 +0200

    This is our first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,32 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from './guards/auth.guard';
 
 const routes: Routes = [
   {
+    path: 'members',
+    loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule),
+canActivate: [AuthGuard]
+  },
+  {
     path: '',
-    loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
+    loadChildren: () => import('./login/login.module').then(m => m.LoginPageModule)
+  },
+  {
+    path: 'register',
+    loadChildren: () => import('./register/register.module').then(m => m.RegisterPageModule)
+  },
+  {
+    path: 'home',
+    loadChildren: () => import('./home/home.module').then(m => m.HomePageModule)
+  },
+  {
+    path: 'about',
+    loadChildren: () => import('./about/about.module').then(m => m.AboutPageModule)
+  },
+  {
+    path: 'songlist',
+    loadChildren: () => import('./songlist/songlist.module').then(m => m.SonglistPageModule)
   }
 ];
 @NgModule({
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,16 +1,13 @@
-// This file can be replaced during build by using the `fileReplacements` array.
-// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
-// The list of file replacements can be found in `angular.json`.
-
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyDcfG35rJ_M31JewjoGBxabF1UHw62GAlk',
+    authDomain: 'devdacticlogin.firebaseapp.com',
+    databaseURL: 'https://devdacticlogin.firebaseio.com',
+    projectId: 'devdacticlogin',
+    storageBucket: 'devdacticlogin.appspot.com',
+    messagingSenderId: '741283632746',
+    appId: '1:741283632746:web:cee1b49d077d11fcf05c13',
+    measurementId: 'G-0CSX5N5NBP'
+  }
 };
-
-/*
- * For easier debugging in development mode, you can import the following file
- * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
- *
- * This import should be commented out in production mode because it will have a negative impact
- * on performance if an error is thrown.
- */
-// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
